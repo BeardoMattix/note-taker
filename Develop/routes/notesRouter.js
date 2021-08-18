be94745f732339851091cfcefce65a3d5b07c9db
Beardo Mattix <christopher.mattix@gmail.com>
2021-08-18 10:41:02 -0400

    setting up get and post routes for notesRouter.js

--- Develop/routes/notesRouter.js
+++ Develop/routes/notesRouter.js
@@ -1,1 +1,37 @@
-const notes = require('express').Router();
+const notes = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+
+// Get route for the info in db.json
+notes.get("/", (req, res) =>
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
+);
+
+// POST Route for submitting a new note
+notes.post("/", (req, res) => {
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text,
+      note_id: uuidv4(),
+    };
+
+    readAndAppend(newNote, "./db/feedback.json");
+
+    const response = {
+      status: "success",
+      body: newFeedback,
+    };
+
+    res.json(response);
+  } else {
+    res.json("Error in posting feedback");
+  }
+});
+
+module.exports = notes;
